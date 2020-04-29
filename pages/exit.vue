3fcecfc1128690160cc2e621ac0c91895424e488
mystic <16651618852@163.com>
2020-04-29 17:05:02 +0800

    '下班'

--- pages/exit.vue
+++ pages/exit.vue
@@ -3,16 +3,16 @@
 </template>
 
 <script>
-    export default {
-      middleware: async ctx => {
-        let {status, data} = await ctx.$axios.get('/users/exit')
-        if(status == 200 && data.code == 0){
-          window.location.href = '/'
-        }
-      },
-      layout: 'blank',
-        name: "exit"
-    }
+  export default {
+    middleware: async ctx => {
+      let {status, data} = await ctx.$axios.get('/users/exit')
+      if(status == 200 && data.code == 0){
+        window.location.href = '/'
+      }
+    },
+    layout: 'blank',
+    name: "exit"
+  }
 </script>
 
 <style scoped>
